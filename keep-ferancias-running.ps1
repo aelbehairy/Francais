f15bc5f93d4e624b8a42a330f4c7b6c89f569979
aelbehairy <aelbehairy@outlook.com>
2026-07-01 09:32:06 +0300

    Update pronunciation

--- keep-ferancias-running.ps1
+++ keep-ferancias-running.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'SilentlyContinue'
 $appDir = 'C:\Users\aelbehairy.SBG-EX\Desktop\French\WebSite\Publish\FromGithub\Ferancias'
-$portOpen = Test-NetConnection -ComputerName '127.0.0.1' -Port 4173 -InformationLevel Quiet
+$portOpen = Test-NetConnection -ComputerName '127.0.0.1' -Port 5555 -InformationLevel Quiet
 if (-not $portOpen) {
   Start-Process -FilePath 'node.exe' -ArgumentList @('server.js') -WorkingDirectory $appDir -WindowStyle Hidden -RedirectStandardOutput 'server.out.log' -RedirectStandardError 'server.err.log'
 }
